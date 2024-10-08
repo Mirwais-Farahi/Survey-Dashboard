f249989436a8df3abe7214bfd1ed2a1cb70dc5fc
Mirwais Farahi <waisfarahi@example.com>
2024-10-08 09:50:50 +0430

    Update

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def load_and_display_data(selected, submitted_after):
                 st.dataframe(df[showData], use_container_width=True)
 
         selected_columns = []
-        column_options = df.columns.tolist()
+        column_options = df.select_dtypes(include=['number']).columns.tolist()
         selected_columns = st.multiselect("Select Columns for Statistical Calculation:", column_options, default=selected_columns)
 
         total_1 = len(df) if len(df) > 0 else 0
